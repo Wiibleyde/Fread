c1fd0282b892cca17787d49febaff2a6cfe8c26e
Wiibleyde <nathan@bonnell.fr>
2025-11-28 12:07:23 +0100

    feat: refactor authentication components and add react-icons support
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ components/auth/SignIn.tsx
@@ -0,0 +1,40 @@
+import { FaApple, FaDiscord, FaGoogle } from "react-icons/fa";
+import { signIn } from "@/auth";
+import { Button } from "../ui/button";
+
+interface SignInProps {
+    provider: "google" | "discord" | "apple";
+}
+
+export function SignIn({ provider }: SignInProps) {
+    let icon: React.ReactNode;
+
+    switch (provider) {
+        case "google":
+            icon = <FaGoogle />;
+            break;
+        case "discord":
+            icon = <FaDiscord />;
+            break;
+        case "apple":
+            icon = <FaApple />;
+            break;
+        default:
+            throw new Error("Unsupported provider");
+    }
+
+    return (
+        <form
+            action={async () => {
+                "use server";
+                await signIn(provider);
+            }}
+        >
+            <Button type="submit" variant="outline" size="lg">
+                {icon}
+                Sign in with{" "}
+                {provider.charAt(0).toUpperCase() + provider.slice(1)}
+            </Button>
+        </form>
+    );
+}
---
+++ components/auth/SignOut.tsx
@@ -0,0 +1,20 @@
+import { signOut } from "@/auth";
+import { Button } from "../ui/button";
+
+export function SignOut() {
+    return (
+        <form
+            action={async () => {
+                "use server";
+                await signOut({
+                    redirectTo: "/",
+                });
+            }}
+            className="mt-4"
+        >
+            <Button type="submit" variant="outline" size="lg" className="bg-red-600 hover:bg-red-700 text-white">
+                Logout
+            </Button>
+        </form>
+    );
+}
