1d149592938a30c4b93efe880e112bfdf0b2ec0a
Matsuel <matheolang@icloud.com>
2025-12-07 21:21:57 +0100

    feat: ajouter une vérification de santé de la base de données avant le démarrage de l'API

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import { env } from "./env";
 import authRouter from "./routes/auth";
 import testRouter from "./routes/test";
+import { dbHealthCheck } from "./utils/db";
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,7 @@ app.get("/status", (_req, res) => {
 app.use("/auth", authRouter);
 app.use("/test", testRouter);
 
-app.listen(env.PORT, () => {
+app.listen(env.PORT, async () => {
+    await dbHealthCheck();
     console.log(`API server running on http://localhost:${env.PORT}`);
 });
